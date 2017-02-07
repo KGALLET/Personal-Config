b27ac53307a65e37437bd57be1f26322da9a22bb
Kevin Gallet <kevingallet@gmail.com>
2017-02-07 11:40:06 +0100

    Update autoenableproxy display message

--- autoEnableProxy.sh
+++ autoEnableProxy.sh
@@ -3,11 +3,10 @@
 SSID=LILLE1
 
 if [[ $(networksetup -getairportnetwork en0 | cut -c 24-) = $SSID ]];
-	then 
-  	echo "Proxy enable."
+	then
+  	echo "Proxy enable in progress.."
     source $HOME/proxy.sh on
 elif [[ $(networksetup -getairportnetwork en0 | cut -c 24-) != $SSID ]]; then
     source $HOME/proxy.sh off
   	echo "Proxy disable."
 fi
-
