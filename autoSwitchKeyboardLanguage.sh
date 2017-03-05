dd5a5df471387ee1ce629e3cdf081b41e0863bf5
Kevin Gallet <kevingallet@gmail.com>
2017-03-05 23:15:32 +0100

    Update to fit with Razer Blackwidow keyboard

--- autoSwitchKeyboardLanguage.sh
+++ autoSwitchKeyboardLanguage.sh
@@ -2,20 +2,26 @@
 
 ACTIVELAYOUT="$(osascript $HOME/Scripts/GetInputSource.scpt)"
 KEYBOARD1=Mini\ Keyboard
-#KEYBOARD2=
+KEYBOARD2=Razer\ BlackWidow
 
-if [[ $(ioreg -p IOUSB -w0 | sed 's/[^o]*o //; s/@.*$//' | grep -v '^Root.*'  | grep "Mini Keyboard") = $KEYBOARD1 ]]; 
+if [[ $(ioreg -p IOUSB -w0 | sed 's/[^o]*o //; s/@.*$//' | grep -v '^Root.*' | grep $KEYBOARD1) = $KEYBOARD1 ]];
 	then
-	echo "Windows keyboard(s) detected, switching Keyboard Layout..."
+	echo "$KEYBOARD1 detected, switching Keyboard Layout..."
 	if [[ ($ACTIVELAYOUT = "French - numerical") ]];
 	then
 		osascript $HOME/Scripts/ChangeLayoutAppleScript.scpt
 	fi
-elif [[ $(ioreg -p IOUSB -w0 | sed 's/[^o]*o //; s/@.*$//' | grep -v '^Root.*'  | grep "Mini Keyboard") != $KEYBOARD1 ]];
+elif [[ $(ioreg -p IOUSB -w0 | sed 's/[^o]*o //; s/@.*$//' | grep -v '^Root.*' | grep $KEYBOARD2) = $KEYBOARD2 ]];
 	then
-  	echo "No Windows keyboard detected, switching to default Keyboard Layout..."
+  	echo "$KEYBOARD2 detected, switching Keyboard Layout..."
+	if [[ ($ACTIVELAYOUT = "French - numerical") ]];
+ 	then
+		osascript $HOME/Scripts/ChangeLayoutAppleScript.scpt
+	fi
+else
+	echo "No Windows Keyboards detected, switching to default Keyboard Layout..."
 	if [[ ($ACTIVELAYOUT = "French-PC") ]];
- 	then 
+ 	then
 		osascript $HOME/Scripts/ChangeLayoutAppleScript.scpt
 	fi
 fi
